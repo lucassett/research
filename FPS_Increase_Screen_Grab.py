e7526b4e12412605f1344fd8034e48b153e0aea9
Lucas Settergren <lucassettergren@gmail.com>
2020-05-31 20:28:39 -0400

    Changes made to save frames

--- FPS_Increase_Screen_Grab.py
+++ FPS_Increase_Screen_Grab.py
@@ -1,6 +1,7 @@
 # import the necessary packages
 from __future__ import print_function
 import datetime
+import os
 class FPS:
 	def __init__(self):
 		# store the start time, end time, and total number of frames
@@ -66,52 +67,28 @@ import imutils
 
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-n", "--num-frames", type=int, default=100,
+ap.add_argument("dir", type = str, help = "Directory to put frames in")
+ap.add_argument("base", type = str, help = "Base file name")
+ap.add_argument("-n", "--num-frames", type=int, default=500,
 	help="# of frames to loop over for FPS test")
-ap.add_argument("-d", "--display", type=int, default=-1,
-	help="Whether or not frames should be displayed")
-args = vars(ap.parse_args())
-
-# grab a pointer to the video stream and initialize the FPS counter
-print("[INFO] sampling frames from webcam...")
-stream = cv2.VideoCapture(0)
-fps = FPS().start()
-# loop over some frames
-while fps._numFrames < args["num_frames"]:
-	# grab the frame from the stream and resize it to have a maximum
-	# width of 400 pixels
-	(grabbed, frame) = stream.read()
-	frame = imutils.resize(frame, width=400)
-	# check to see if the frame should be displayed to our screen
-	if args["display"] > 0:
-		cv2.imshow("Frame", frame)
-		key = cv2.waitKey(1) & 0xFF
-	# update the FPS counter
-	fps.update()
-# stop the timer and display FPS information
-fps.stop()
-print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
-print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
-# do a bit of cleanup
-stream.release()
-cv2.destroyAllWindows()
+args = ap.parse_args()
+if not os.path.exists(args.dir):
+	os.mkdir(args.dir)
 
 
+frames = []
 # created a *threaded* video stream, allow the camera sensor to warmup,
 # and start the FPS counter
 print("[INFO] sampling THREADED frames from webcam...")
 vs = WebcamVideoStream(src=0).start()
 fps = FPS().start()
 # loop over some frames...this time using the threaded stream
-while fps._numFrames < args["num_frames"]:
+while fps._numFrames < args.num_frames:
 	# grab the frame from the threaded video stream and resize it
 	# to have a maximum width of 400 pixels
 	frame = vs.read()
 	frame = imutils.resize(frame, width=400)
-	# check to see if the frame should be displayed to our screen
-	if args["display"] > 0:
-		cv2.imshow("Frame", frame)
-		key = cv2.waitKey(1) & 0xFF
+	frames.append(frame)
 	# update the FPS counter
 	fps.update()
 # stop the timer and display FPS information
@@ -119,7 +96,12 @@ fps.stop()
 print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
 print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
 
+count = 1
+for frame in frames:
+        fname = os.path.join(args.dir, args.base+ str(count) + ".png")
+        cv2.imwrite(fname,frame)
+        count += 1
+
 # do a bit of cleanup
-cv2.destroyAllWindows()
 vs.stop()
 
